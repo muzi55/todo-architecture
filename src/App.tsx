4509583b19746363d5317bb61290d1eaea4762d0
muzi5@kakao.com <muzi5@kakao.com>
2024-11-06 18:15:47 +0900

    readme 작성

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,16 @@ import Todo from "./components/Todo";
 import TodoForm from "./components/TodoForm";
 import { ITodo } from "./libs/type";
 
+const initialTodo: ITodo[] = [
+  {
+    id: new Date(),
+    title: "투두앱 만들기",
+    content: "리액트와 타입스크립트로 투두앱 만들기",
+  },
+];
+
 function App() {
-  const [todo, setTodo] = useState<ITodo[]>([]);
+  const [todo, setTodo] = useState<ITodo[]>(initialTodo);
   return (
     <>
       <h1>투두앱</h1>

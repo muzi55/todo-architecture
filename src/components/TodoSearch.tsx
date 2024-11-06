96a8430124ec2f38e883dfd15cb5c6e9c1078f68
muzi5@kakao.com <muzi5@kakao.com>
2024-11-06 18:30:46 +0900

    search 기능

--- src/components/TodoSearch.tsx
+++ src/components/TodoSearch.tsx
@@ -1,7 +1,34 @@
+import { useRef, useState } from "react";
 import { ITodoProps } from "../libs/type";
 
-function TodoSearch({ todo, setTodo }: ITodoProps) {
-  return <div>TodoSearch</div>;
+function TodoSearch({ todo }: ITodoProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [searchResults, setSearchResults] = useState(todo);
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const keyword = inputRef.current?.value || "";
+
+    setSearchResults(keyword ? todo.filter((el) => el.title.includes(keyword) || el.content.includes(keyword)) : []);
+  };
+
+  return (
+    <div>
+      <h2>투두 검색</h2>
+
+      <form onSubmit={onSubmit}>
+        <input ref={inputRef} type="text" />
+        <button>검색</button>
+      </form>
+
+      <ul>
+        {searchResults.map((el) => (
+          <li key={el.id}>{el.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default TodoSearch;
